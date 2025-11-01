8a855e2c62496d46086ddb086a06c9e4bc6bfab2
Sendiko <sarangtawon897@gmail.com>
2025-11-01 12:20:31 +0700

    chore: Update database schema and integrate PeopleScreen with ViewModel and new resources

--- composeApp/src/commonMain/kotlin/id/my/sendiko/roominkmp/roominkmp/App.kt
+++ composeApp/src/commonMain/kotlin/id/my/sendiko/roominkmp/roominkmp/App.kt
@@ -13,8 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import id.my.sendiko.roominkmp.roominkmp.person.presentation.PeopleScreen
+import id.my.sendiko.roominkmp.roominkmp.person.presentation.PeopleViewModel
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 import roominkmp.composeapp.generated.resources.Res
 import roominkmp.composeapp.generated.resources.compose_multiplatform
@@ -23,27 +26,11 @@ import roominkmp.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        val viewModel = koinViewModel<PeopleViewModel>()
+        val state by viewModel.state.collectAsState()
+        PeopleScreen(
+            state = state,
+            onEvent = viewModel::onEvent,
+        )
     }
 }
